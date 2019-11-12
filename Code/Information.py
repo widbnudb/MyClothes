a4625ba3cfa0a2be9818684c51f739830db7027a
Elizabeth Lemeshko <lzbth.lemeshko@gmail.ru>
2019-11-12 22:44:15 +0300

    Menu, Info and Exit gui are were added

---
+++ Code/Information.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Information.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(471, 386)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 181, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 60, 451, 192))
+        self.textBrowser.setObjectName("textBrowser")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(342, 277, 111, 41))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 471, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MyClothes"))
+        self.label.setText(_translate("MainWindow", "Information"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">This app for recognizing photos with cloth and saving in your virtual wardrobe.</span></p>\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">In &quot;Load photo&quot; you can choose photo and load it in recognizer.</span></p>\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">In &quot;Wardrobe&quot; you can choose folder and watch elements of clothes, which you loaded before.</span></p>\n"
+"<p align=\"justify\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "Exit"))
---
+++ Code/MyClothes.py
@@ -0,0 +1,51 @@
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
+import Menu  # Это наш конвертированный файл дизайна
+import Information
+
+
+class MenuWindow(QtWidgets.QMainWindow, Menu.Ui_MainWindow):
+
+    info_window = ""
+
+    def __init__(self, parent=None):
+        super(MenuWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        self.pushButton_3.clicked.connect(self.from_menu_to_info)
+        self.pushButton_4.clicked.connect(self.close_app)
+
+    def from_menu_to_info(self):
+        self.info_window = InfoWindow()
+        self.info_window.show()
+        self.close()
+
+    def close_app(self):
+        self.close()
+
+
+class InfoWindow(QtWidgets.QMainWindow, Information.Ui_MainWindow):
+
+    out_menu_window = ""
+
+    def __init__(self, parent=None):
+        super(InfoWindow, self).__init__(parent)
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+
+        self.pushButton.clicked.connect(self.from_info_to_menu)
+
+    def from_info_to_menu(self):
+        self.out_menu_window = MenuWindow()
+        self.out_menu_window.show()
+        self.close()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    menu_window = MenuWindow()  # Создаём объект класса ExampleApp
+    menu_window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
